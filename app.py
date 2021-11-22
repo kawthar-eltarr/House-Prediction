cbe94ddc6cb2b5253edc9f6232dd2d80ea46fcaf
eltarr-kawther <kawther.eltarr@laplateforme.io>
2021-11-22 11:18:29 +0100

    organizing into notebooks folder

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import dash
 
-
-
-app = dash.Dash(__name__,suppress_callback_exceptions=True)
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
 app.title = "House Prediction"

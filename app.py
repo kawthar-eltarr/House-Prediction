a2cd818046ca0be5c6df7c4670949ca64e8c56f2
eltarr-kawther <kawther.eltarr@laplateforme.io>
2021-11-24 16:56:20 +0100

    still trying to deploy

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import dash
 
-BS = r"C:/Users/straw/Desktop/AIS2/House-Prediction/assets/style.css"
+BS = "https://raw.githubusercontent.com/eltarr-kawther/House-Prediction/main/assets/style.css"
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[BS])
 server = app.server
